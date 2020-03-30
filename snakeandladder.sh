f01d702b25ceb7844838c0a13b60e340517b4976
vivek kumar <vivekhaarit@gmail.com>
2020-03-30 13:19:33 +0530

    [vivek]Refract:removed bugs

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
 
 echo "welcome to SNAKE & LADDER"
-position=0;
-playcount=0;
+playerOnePosition=0;
+playerTwoPosition=0;
+playerOneMoves=0;
+playerTwoMoves=0;
+
+#who gets the first chance
+player=$(($RANDOM%2+1))
+
+ShowPlayer(){
+    if [ $player -eq 1 ]
+    then
+        echo "player $player is playing."
+    else
+        echo "player $player is playing."
+    fi
+}
+SwitchPlayer(){
+
+    if [ $player -eq 1 ]
+    then
+        playerOneMoves=$(($playerOneMoves+$1))
+        player=2;
+    else
+        playerTwoMoves=$(($playerTwoMoves+$1))
+        player=1;
+    fi
+}
 RollTheDice(){
     dice=$(($RANDOM%6+1)) 
 }
-while [ position -le 100 ]
+
+while [ $playerOnePosition -le 100 ] && [ $playerTwoPosition -le 100 ]
 do
+    if [ $player -eq 1 ]
+    then
+        position=$playerOnePosition
+        playerMoves=$playerOneMoves
+    else
+        position=$playerTwoPosition
+        playerMoves=$playerTwoMoves
+    fi
+
     CheckingForOptions(){
-        option=$(($RANDOM%2))
+        option=$(($RANDOM%3))
         if [ $option -eq 0 ]
         then
             NoPlay
@@ -22,37 +57,55 @@ do
     }
     NoPlay(){
         #Stay in the same position
-        echo "You chose not to play this move"
+        ShowPlayer
+        echo "I'm not playing this move"
+        playerMoves=$(($playerMoves+1))
+        echo "Total moves so far: " $playerMoves
+        SwitchPlayer $playerMoves
     }
     Ladder(){
-        if [ $(($position+$dice)) -gt 100 ]
+        position=$(($position + $dice))
+        if [ $position -gt 100 ]
         then
             #roll the dice again
-            echo "you don't have enough places to move.Roll the dice again."
+            ShowPlayer
+            echo "I don't have enough places to move.Roll the dice again."
             echo "....."
-            sleep 1
-            ((playcount++))
+            playerMoves=$(($playerMoves+1))
+            echo "Total moves so far: " $playerMoves
+            SwitchPlayer $playerMoves
             RollTheDice
         else
-            position=$(($position+$dice))
+            ShowPlayer
+            position=$(($position + $dice))
             echo "you got ladder,your current position is.."
             echo "$position"
-            ((playcount++))
+            playerMoves=$(($playerMoves+1))
+            echo "Total moves so far: " $playerMoves
+            SwitchPlayer $playerMoves
             RollTheDice
         fi
     }
     Snake(){
         #move behind
-        position=$(($position-$dice))
+        ShowPlayer
+        position=$(("$position" - "$dice"))
         if [ $position -lt 0 ]
         then
             position=0;
             echo "you can't move below zero."
+            echo "Total moves so far: " $playerMoves
+            SwitchPlayer $playerMoves
+            RollTheDice
+        else
+            echo "you caught snake, your current position is..."
+            echo "$positon"
+            ((playerMoves++))
+            echo "Total moves so far: " $playerMoves
+            SwitchPlayer $playerMoves
             RollTheDice
         fi
-        echo "you caught snake, your current position is..."
-        echo "$positon"
-        ((playcount++))
-        RollTheDice
     }
+    RollTheDice
+    CheckingForOptions
 done
